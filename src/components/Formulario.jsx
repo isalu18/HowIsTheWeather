74fb64f91539da7fa68c47b5d8a35ef5cfba0f12
isalu18 <luke_shakz@hotmail.com>
2021-04-02 14:39:48 -0700

    Creating Header and form

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const Formulario = () => {
+
+    //State del formulario
+    const [ busqueda, guardarBusqueda ] = useState({
+        ciudad:'',
+        pais:''
+    });
+
+    //Extraer ciudad y pais
+    const { ciudad, pais } = busqueda;
+
+    //Funcion para colocar los elementos en el State
+    const handleChange = e => {
+        //Actualizar el State
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    return ( 
+        <form>
+            <div className="input-field col s12">
+                <input
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    value={ ciudad }
+                    onChange={ handleChange }
+                />
+                <label htmlFor="ciudad">Ciudad: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <select 
+                    name="pais" 
+                    id="pais"
+                    value={ pais }
+                    onChange={ handleChange }
+                >
+                    <option value="">-- Seleccione un pais --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                </select>
+                <label htmlFor="pais">Pais: </label>
+            </div>
+        </form>
+     );
+}
+ 
+export default Formulario;
